e9c93443de6ae90bfcdb8a2b4c95f0908832d404
Snickmax <victor.2002.espinoza@gmail.com>
2024-05-21 12:42:51 -0400

    test back y front

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,16 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/')
-def index():
-    return "¡Bienvenido a NetflixBeta!"
+# Ruta para obtener recomendaciones de películas
+@app.route('/recommendations')
+def get_recommendations():
+    # Lógica para obtener recomendaciones de películas del usuario con el ID proporcionado
+    # Aquí puedes ejecutar consultas en la base de datos Neo4j y procesar los resultados
+    return { "cartelera": [ {"title": "Movie 1", "rating": 4.5} ,
+        {"title": "Movie 2", "rating": 4.0},
+        {"title": "Movie 3", "rating": 3.5}]
+        }
 
 if __name__ == '__main__':
     app.run(debug=True)
