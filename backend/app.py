9abdd38342b9c60c052cf9f162b659e0cb610265
Snickmax <victor.2002.espinoza@gmail.com>
2024-06-08 20:22:20 -0400

    recomendaciones

--- backend/app.py
+++ backend/app.py
@@ -19,34 +19,36 @@ neo4j_password = os.getenv("NEO4J_PASSWORD")
 # Crear una instancia de Driver para interactuar con la base de datos Neo4j
 driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_user, neo4j_password))
 
-def get_movie_nuevos_lanzamientos(tx):
+#   --- Consultas movies---
+# mandamos los nuevos lanzamientos
+def get_movie_nuevos_lanzamientos(tx, usuario):
     query = """
     MATCH (m:Pelicula)
+    WHERE NOT EXISTS {
+        MATCH (:Usuario {nombre: $usuario})-[:VIO]->(m)
+    }
     RETURN m.titulo AS title, m.calificacion_promedio AS rating, m.año AS año, m.caratula as img
     ORDER BY año DESC
     LIMIT 10
     """
-    result = tx.run(query)
+    result = tx.run(query, usuario=usuario)
     return [{"title": record["title"], "rating": record["rating"], "año": record["año"], "img": record["img"]} for record in result]
 
-def get_movie_mas_vistas(tx):
+# mandamos las mas vistas
+def get_movie_mas_vistas(tx, usuario):
     query = """
     MATCH (m:Pelicula)
+    WHERE NOT EXISTS {
+        MATCH (:Usuario {nombre: $usuario})-[:VIO]->(m)
+    }
     RETURN m.titulo AS title, m.calificacion_promedio AS rating, m.año AS año, m.caratula as img
     ORDER BY rating DESC
     LIMIT 10
     """
-    result = tx.run(query)
+    result = tx.run(query, usuario=usuario)
     return [{"title": record["title"], "rating": record["rating"], "año": record["año"], "img": record["img"]} for record in result]
 
-def get_peliculas_vistas(tx, usuario):
-    query = """
-    MATCH (u:Usuario {nombre: $usuario})-[:VIO]->(m:Pelicula)
-    RETURN m.titulo AS title, m.calificacion_promedio AS rating, m.caratula as img
-    """
-    result = tx.run(query, usuario=usuario)
-    return [{"title": record["title"], "rating": record["rating"], "img": record["img"]} for record in result]
-
+# mandamos las peliculas que quiere ver
 def get_peliculas_quiere_ver(tx, usuario):
     query = """
     MATCH (u:Usuario {nombre: $usuario})-[:QUIERE_VER]->(m:Pelicula)
@@ -55,6 +57,90 @@ def get_peliculas_quiere_ver(tx, usuario):
     result = tx.run(query, usuario=usuario)
     return [{"title": record["title"], "rating": record["rating"], "img": record["img"]} for record in result]
 
+#   --- Recomendacion de peliculas segun una que vio tanto recomendacion por la pelicula y generos ---
+# peliculas que ya vio
+def get_peliculas_vistas(tx, usuario):
+    query = """
+    MATCH (u:Usuario {nombre: $usuario})-[:VIO]->(m:Pelicula)
+    RETURN m.titulo AS title, m.calificacion_promedio AS rating, m.caratula as img
+    """
+    result = tx.run(query, usuario=usuario)
+    return [{"title": record["title"], "rating": record["rating"], "img": record["img"]} for record in result]
+
+# genero de las pelicula vista
+def get_generos_de_pelicula_vista(tx, usuario, titulo):
+    query = """
+    MATCH (:Usuario {nombre: $usuario})-[:VIO]->(p:Pelicula {titulo: $titulo})-[:ES_DE]->(g:Genero)
+    RETURN g.nombre AS genero
+    """
+    result = tx.run(query, usuario=usuario, titulo=titulo)
+    return [record["genero"] for record in result]
+
+# Peliculas de los mismos generos mas de 1
+def recomendar_peliculas_mismos_generos(tx, usuario, generos):
+    query = """
+    MATCH (m:Pelicula)-[:ES_DE]->(g:Genero)
+    WHERE g.nombre IN $generos AND NOT EXISTS {
+        MATCH (:Usuario {nombre: $usuario})-[:VIO]->(m)
+    }
+    WITH m, COLLECT(g.nombre) AS movieGenres
+    WHERE ALL(genre IN $generos WHERE genre IN movieGenres) AND SIZE(movieGenres) = SIZE($generos)
+    RETURN m.titulo AS title, m.calificacion_promedio AS rating, m.año AS año, m.caratula as img
+    ORDER BY rating DESC
+    LIMIT 10
+    """
+    result = tx.run(query, usuario=usuario, generos=generos)
+    return [{"title": record["title"], "rating": record["rating"], "año": record["año"], "img": record["img"]} for record in result]
+
+# Peliculas de generos independientes
+def recomendar_peliculas_generos_independientes(tx, usuario, generos):
+    query = """
+    MATCH (m:Pelicula)-[:ES_DE]->(g:Genero)
+    WHERE g.nombre IN $generos AND NOT EXISTS {
+        MATCH (:Usuario {nombre: $usuario})-[:VIO]->(m)
+    }
+    RETURN DISTINCT m.titulo AS title, m.calificacion_promedio AS rating, m.año AS año, m.caratula as img
+    ORDER BY rating DESC
+    LIMIT 10
+    """
+    result = tx.run(query, usuario=usuario, generos=generos)
+    return [{"title": record["title"], "rating": record["rating"], "año": record["año"], "img": record["img"]} for record in result]
+
+# Peliculas de generos independientes
+def get_generos_mas_vistos_por_usuario(tx, usuario):
+    query = """
+    MATCH (:Usuario {nombre: $usuario})-[:VIO]->(:Pelicula)-[:ES_DE]->(g:Genero)
+    RETURN g.nombre AS genero, COUNT(*) AS count
+    ORDER BY count DESC
+    """
+    result = tx.run(query, usuario=usuario)
+    return [record["genero"] for record in result]
+
+def get_generos_mas_vistos_por_usuario2(tx, usuario):
+    query = """
+    MATCH (:Usuario {nombre: $usuario})-[:VIO]->(:Pelicula)-[:ES_DE]->(g:Genero)
+    RETURN g.nombre AS genero, COUNT(*) AS count
+    ORDER BY count DESC
+    """
+    result = tx.run(query, usuario=usuario)
+    return [{"genero": record["genero"], "count": record["count"]} for record in result]
+
+def get_recomendaciones_para_ti(tx, usuario, generos_mas_vistos):
+    query = """
+    MATCH (m:Pelicula)
+    WHERE NOT EXISTS { (:Usuario {nombre: $usuario})-[:VIO]->(m) }
+    OPTIONAL MATCH (m)-[:ES_DE]->(g:Genero)
+    WITH m, $generos_mas_vistos AS generos_mas_vistos, COLLECT(g.nombre) AS pelicula_generos
+    WITH m, generos_mas_vistos, [gen IN generos_mas_vistos WHERE gen.genero IN pelicula_generos | gen.count] AS pesos
+    WITH m, REDUCE(s = 0, p IN pesos | s + p) AS peso
+    RETURN m.titulo AS title, m.calificacion_promedio AS rating, m.año AS año, m.caratula AS img, COALESCE(peso, 0) AS peso
+    ORDER BY peso DESC, rating DESC
+    LIMIT 10
+    """
+    result = tx.run(query, usuario=usuario, generos_mas_vistos=generos_mas_vistos)
+    return [{"title": record["title"], "rating": record["rating"], "año": record["año"], "img": record["img"], "peso": record["peso"]} for record in result]
+
+#   --- Login ---
 def verificar_usuario(tx, usuario, password):
     query = """
     MATCH (u:Usuario {nombre: $usuario, passd: $password})
@@ -103,23 +189,123 @@ def login():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/nuevoslanzamientos')
-def recommendations():
+#   --- recomendaciones ---
+
+"""
+nuevos_lanzamientos():
+    Función: Esta ruta devuelve las últimas películas lanzadas que el usuario aún no ha visto.
+    
+    Respuesta: Devuelve una lista de hasta 10 películas ordenadas por año de lanzamiento, con su título, calificación promedio, año y carátula.
+"""
+@app.route('/nuevoslanzamientos', methods=['GET'])
+def nuevos_lanzamientos():
+    usuario = request.args.get('usuario')
+    if not usuario:
+        return jsonify({"error": "Usuario no proporcionado"}), 400
+
     try:
         with driver.session() as session:
-            results = session.read_transaction(get_movie_nuevos_lanzamientos)
+            results = session.read_transaction(get_movie_nuevos_lanzamientos, usuario)
         return jsonify(results)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/masvistas')
-def nuevos_lanzamientos():
+"""
+mas_vistas():
+    Función: Esta ruta devuelve las películas más vistas que el usuario aún no ha visto.
+    Respuesta: Devuelve una lista de hasta 10 películas ordenadas por calificación promedio, con su título, calificación promedio, año y carátula.
+"""
+@app.route('/masvistas', methods=['GET'])
+def mas_vistas():
+    usuario = request.args.get('usuario')
+    if not usuario:
+        return jsonify({"error": "Usuario no proporcionado"}), 400
+    
     try:
         with driver.session() as session:
-            results = session.read_transaction(get_movie_mas_vistas)
+            results = session.read_transaction(get_movie_mas_vistas, usuario)
         return jsonify(results)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
+"""
+recomendar_por_generos():
+    Función: Esta ruta recomienda películas al usuario basadas en los géneros que más ha visto.
+    Respuesta: Devuelve un diccionario donde las claves son los géneros más vistos por el usuario y los valores son listas de hasta 10 películas recomendadas por cada género. Cada película incluye su título, calificación promedio, año y carátula.
+"""
+@app.route('/recomendarporgeneros', methods=['GET'])
+def recomendar_por_generos():
+    usuario = request.args.get('usuario')
+    if not usuario:
+        return jsonify({"error": "Usuario no proporcionado"}), 400
+    
+    try:
+        with driver.session() as session:
+            generos_mas_vistos = session.read_transaction(get_generos_mas_vistos_por_usuario, usuario)
+            if not generos_mas_vistos:
+                return jsonify({"error": "No se encontraron géneros para el usuario proporcionado"}), 404
+            
+            recomendaciones = {}
+            for genero in generos_mas_vistos:
+                peliculas_recomendadas = session.read_transaction(recomendar_peliculas_generos_independientes, usuario, [genero])
+                if peliculas_recomendadas:
+                    recomendaciones[genero] = peliculas_recomendadas
+
+            return jsonify(recomendaciones)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+"""
+recomendar_por_pelicula():
+    Función: Esta ruta recomienda películas al usuario basadas en una película específica que ha visto.
+    Respuesta: Devuelve una lista de hasta 10 películas recomendadas. Incluye las películas del mismo género que la película proporcionada, seguidas de películas de géneros independientes. Cada película incluye su título, calificación promedio, año y carátula.
+"""
+@app.route('/recomendarporpelicula', methods=['GET'])
+def recomendar_por_pelicula():
+    usuario = request.args.get('usuario')
+    titulo = request.args.get('titulo')
+    if not usuario or not titulo:
+        return jsonify({"error": "Usuario o título de película no proporcionado"}), 400
+    
+    try:
+        with driver.session() as session:
+            generos = session.read_transaction(get_generos_de_pelicula_vista, usuario, titulo)
+            if not generos:
+                return jsonify({"error": "No se encontraron géneros para la película proporcionada"}), 404
+
+            mismas_generos_peliculas = session.read_transaction(recomendar_peliculas_mismos_generos, usuario, generos)
+            combinadas_peliculas = mismas_generos_peliculas
+            if len(mismas_generos_peliculas) < 10:
+                generos_independientes_peliculas = session.read_transaction(recomendar_peliculas_generos_independientes, usuario, generos)
+                combinadas_peliculas += generos_independientes_peliculas
+                combinadas_peliculas = combinadas_peliculas[:10]  # Limitar a 10 resultados
+
+            return jsonify(combinadas_peliculas)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+"""
+recomendaciones_para_ti():
+    Función: Esta ruta proporciona recomendaciones personalizadas al usuario basadas en sus géneros de películas más vistos.
+    Respuesta: Devuelve una lista de hasta 10 películas recomendadas para el usuario. Las películas están seleccionadas en función de los géneros más vistos por el usuario. Cada película incluye su título, calificación promedio, año y carátula.
+"""
+@app.route('/recomendacionesparati', methods=['GET'])
+def recomendaciones_para_ti():
+    usuario = request.args.get('usuario')
+    if not usuario:
+        return jsonify({"error": "Usuario no proporcionado"}), 400
+    
+    try:
+        with driver.session() as session:
+            generos_mas_vistos = session.read_transaction(get_generos_mas_vistos_por_usuario2, usuario)
+            if not generos_mas_vistos:
+                return jsonify({"error": "No se encontraron géneros para el usuario proporcionado"}), 404
+            
+            recomendaciones = session.read_transaction(get_recomendaciones_para_ti, usuario, generos_mas_vistos)
+
+            return jsonify(recomendaciones)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
